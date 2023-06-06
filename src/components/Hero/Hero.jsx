8cca09bc7d1b4a477e7c8cb113fd0942aac589b1
adityaprasad-op <aditya.prasad2002@gmail.com>
2023-06-06 15:31:32 -0400

    First commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import './Hero.css'
+import {HiLocationMarker} from 'react-icons/hi'
+import CountUp from 'react-countup'
+
+const Hero = () => {
+  return (
+    <section className="hero-wrapper">
+      <div className="padding innerWidth flexCenter hero-container">
+        <div className="flexColStart hero-left">
+          <div className="hero-title">
+          <div className="orange-circle"></div>
+            <h1>
+              Discover<br/>
+              Most Suitable<br/>
+              Property
+            </h1>
+          </div>
+          <div className="flexColStart hero-des">
+            <span className='secondaryText'>Lorem icitempora chudc csuc dschcn!</span>
+            <span className='secondaryText'>Lorem ipsum icsd  cdc wcl wclhwmos?</span>
+          </div>
+          <div className="flexCenter search-bar">
+            <HiLocationMarker color="var(--blue)" size={25}/>
+            <input type='text'/>
+            <button className="button">Search</button>
+          </div>
+          <div className="flexCenter stats">
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp start={8800} end={9000} duration={4}/><span>+</span>
+              </span><span className='secondaryText'>Premium Products</span>
+            </div>
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp start={1950} end={2000} duration={4}/><span>+</span>
+              </span><span className='secondaryText'>Happy Customers</span>
+            </div>
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp end={28} /><span>+</span>
+              </span><span className='secondaryText'>Award wining </span>
+            </div>
+          </div>
+        </div>
+        <div className="flexCenter hero-right">
+          <div className="image-container">
+            <img src='./hero-image.png' alt=""/> 
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
---
+++ src/components/Value/Value.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import{
+    Accordion,
+    AccordionItem,
+    AccordionItemHeading,
+    AccordionItemButton,
+    AccordionItemPanel,
+    AccordionItemState
+} from 'react-accessible-accordion'
+import "react-accessible-accordion/dist/fancy-example.css"
+import './Value.css'
+
+const Value = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default Value
